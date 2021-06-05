4ed8536329d2ae37f9aafea72593cb41ef25a6d0
fullStDev <77523622+fullStDev@users.noreply.github.com>
2021-06-05 16:52:39 +0400

    Add files via upload

---
+++ Exam/ErmakovTicket№7/Variable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErmakovTicket_7
+{
+    class Variable
+    {
+        double enter = 0;
+        double tempMax = 0;
+
+        public void Metod()
+        {
+            Console.WriteLine("Введите 5 значений");
+            for (int i = 0; i < 5; i++)
+            {
+                this.enter = Convert.ToDouble(Console.ReadLine());
+
+                if (this.tempMax < this.enter)
+                {
+                    this.tempMax = this.enter;
+                }
+            }
+            Console.WriteLine("Максимальное значение = " + this.enter);
+            Console.ReadLine();
+        }
+    }    
+}
